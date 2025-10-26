6b2c6721915867c2140422ee7afd1e97b743cd01
Ashwin Raikar <ashwin.raikar77@gmail.com>
2025-10-26 13:59:01 -0400

    Added initial training files

---
+++ train_model.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH -A pfw-cs
+#SBATCH --qos=standby
+#SBATCH -o debug/train_out.out
+#SBATCH -e debug/train_err.err
+#SBATCH --nodes=1
+#SBATCH --partition=a100-40gb
+#SBATCH --cpus-per-task=32
+#SBATCH --gres=gpu:1
+#SBATCH --time=04:00:00
+##SBATCH --mail-type=ALL
+#SBATCH --mail-user=raikaa01@pfw.edu
+#SBATCH --mem=30G
+
+DATASET_NAME=$1
+
+# Activate your virtual environment
+source /scratch/gilbreth/raikaa01/Projects/Mistral/venv/bin/activate
+
+# pip3 install -r requirements.txt
+
+
+cd "/scratch/gilbreth/raikaa01/Projects/WorshipFlow"
+
+# Run the training script
+python train_worship_flow.py --splits_dir ./nsynth-splits --output_dir ./models/worship_flow --epochs 30 --batch_size 64
