c8067da62eaa64c6c8153f66e8e33d55ed6f296d
Zack_M <zackmcgill@aol.com>
2025-10-27 12:33:23 +0000

    Implement analyzeFiles view wih dummy inference implementation to assist frontend development in making the api call and displaing the results.

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 
 # Create your views here.
-# def functionName(_):
-#     return JsonResponse({"ok": True})
+
+
+def getInstrumentsFromFile(file):
+    # Dummy implementation for illustration
+    # Will be replaced by model inference logic
+    return ["guitar", "piano", "drums"]
+
+
+def analyzeFiles(httpRequest):
+    uploadedFiles = httpRequest.FILES.getlist("file")
+    if not uploadedFiles:
+        return JsonResponse({"error": "No files uploaded"}, status=400)
+    instrumentLists = []
+    for uploadedFile in uploadedFiles:
+        # Perform analysis on the uploaded files
+        instrumentList = getInstrumentsFromFile(uploadedFile)
+        instrumentLists.append(instrumentList)
+
+    return JsonResponse({"instruments": instrumentLists}, status=200)
